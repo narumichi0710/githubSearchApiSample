823049905d9f529a7c9bf2bd1b9584bef6e42db1
narumichikubo <kubo@arsaga.jp>
2021-12-26 23:50:27 +0900

    メイン画面を表示するところまで作成, Internet Permissionを追加

---
+++ app/src/main/java/com/narumichi/githubsearchapisample/ui/ListSearchFragment.kt
@@ -0,0 +1,8 @@
+package com.narumichi.githubsearchapisample.ui
+
+
+import androidx.fragment.app.Fragment
+import com.narumichi.githubsearchapisample.R
+
+class ListSearchFragment : Fragment(R.layout.list_search_fragment) {
+}
