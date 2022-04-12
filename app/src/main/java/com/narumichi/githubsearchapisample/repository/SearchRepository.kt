b852a999e2ac83c0d0bfec1b3fa048673b7867ea
narumichikubo <narumichi7373@gmail.com>
2022-04-12 17:21:02 +0900

    add comment out

--- app/src/main/java/com/narumichi/githubsearchapisample/repository/SearchRepository.kt
+++ app/src/main/java/com/narumichi/githubsearchapisample/repository/SearchRepository.kt
@@ -5,8 +5,10 @@ import com.narumichi.githubsearchapisample.model.ResponseListSearch
 import retrofit2.Response
 import javax.inject.Inject
 
+// コンストラクタにApiHelperImplの依存性を注入しているため、インスタンスを新しく生成する必要は無い
 class SearchRepository @Inject constructor(
     private val apiHelperImpl: ApiHelperImpl
 ) {
+    // API Call
     suspend fun getListSearch(value: String): Response<ResponseListSearch> = apiHelperImpl.getListSearch(value)
 }
