f4c29393cf810f4e53749b5e2be1a08dd47738e1
narumichikubo <narumichi7373@gmail.com>
2022-04-12 15:00:19 +0900

    add Fragment extension for compose

---
+++ app/src/main/java/com/narumichi/githubsearchapisample/other/Fragment.kt
@@ -0,0 +1,17 @@
+package com.narumichi.githubsearchapisample.other
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+fun Fragment.composable(fragmentId: Int, content: @Composable () -> Unit): View = ComposeView(requireContext()).apply {
+    id = fragmentId
+    layoutParams = ViewGroup.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    setContent(content)
+}
